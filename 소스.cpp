0fd51cedeec29745de9648420d26058f8ab5f766
KANGYOOHA <wouiomk@daum.net>
2021-09-17 09:35:48 +0900

    수정

---
+++ 소스.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int MyName = 0;
+	float HP = 0.1242f;
+	char y = 'Y';
+
+	cout << MyName << endl;
+	cout << HP<< endl;
+	cout << y << endl;
+
+}
